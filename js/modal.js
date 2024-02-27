44f09f3d36f2670c433ad0041e268d0fd090c756
ReynaAlis <alaingaranzha@gmail.com>
2024-02-27 14:46:17 +0400

    homework

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,8 @@
 const modal = document.querySelector('.search-model');
 const modalBtn = document.querySelector('.icon_search');
 const modalClose = modal.querySelector('.search-close-switch');
+const searchInput = document.getElementById('search-input');
+let timeoutId;
 
 
 modalBtn.addEventListener('click', () => {
@@ -12,4 +14,10 @@ modalClose.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-
+searchInput.addEventListener('input', () => {
+    clearTimeout(timeoutId); 
+    timeoutId = setTimeout(() => {
+        console.log('Введенные данные:', searchInput.value.trim()); 
+        searchInput.value = '';
+    }, 500);
+});
