53ed450fe0f5873bb4a60d884a26414d1d55d36d
ReynaAlis <alaingaranzha@gmail.com>
2024-02-27 17:15:19 +0400

    day_2

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,27 @@
+const modal = () => {
+    const modal = document.querySelector('.search-model');
+    const modalBtn = document.querySelector('.icon_search');
+    const modalClose = modal.querySelector('.search-close-switch');
+    const searchInput = document.getElementById('search-input');
+    let timeoutId;
 
-const modal = document.querySelector('.search-model');
-const modalBtn = document.querySelector('.icon_search');
-const modalClose = modal.querySelector('.search-close-switch');
-const searchInput = document.getElementById('search-input');
-let timeoutId;
 
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
 
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+    modalClose.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
 
-modalClose.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
+    searchInput.addEventListener('input', () => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            console.log('Введенные данные:', searchInput.value.trim());
+            searchInput.value = '';
+        }, 500);
+    });
+}
 
-searchInput.addEventListener('input', () => {
-    clearTimeout(timeoutId); 
-    timeoutId = setTimeout(() => {
-        console.log('Введенные данные:', searchInput.value.trim()); 
-        searchInput.value = '';
-    }, 500);
-});
+modal()
+
